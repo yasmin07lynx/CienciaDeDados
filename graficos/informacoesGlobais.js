20a1de69e8dc904e45c1048b1973666e93d7e33c
yas <105318763+yasmin07lynx@users.noreply.github.com>
2024-10-15 07:57:41 -0300

    qwsertyuiop

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,10 +1,14 @@
 
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
 async function visualizarInformacoesGlobais (){
 const res = await fetch (url)
 const dados = await res.json()
 const paragrafo = document.createElement ('p')
 paragrafo.classList.add('graficos-container_texto')
 paragrafo.innerHTML = `Voce sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectados} estão conectados em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
-console.log(paragrafo)
+
+const container = document.getElementById( 'graficos-container')
+container.appendChild(paragrafo)
 }
+visualizarInformacoesGlobais()
