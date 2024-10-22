9d5172c5b2a000b3b60ab47ae0ee3c7c1c87cea1
yas <105318763+yasmin07lynx@users.noreply.github.com>
2024-10-22 08:03:55 -0300

    trewa

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,6 +4,8 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function visualizarInformacoesGlobais (){
 const res = await fetch (url)
 const dados = await res.json()
+const horas = parseInt (dados.tempo_medio)
+const minutos = Math.round ((dados.tempo_medio - horas) *100)
 const paragrafo = document.createElement ('p')
 paragrafo.classList.add('graficos-container__texto')
 paragrafo.innerHTML = `Voce sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectados} estão conectados em alguma rede social e passam em média ${dados.tempo_medio} horas e <span>${minutos}</span> minutos conectadas.`
