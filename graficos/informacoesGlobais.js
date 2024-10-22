c83ae8ec4a4f23234abca91a7c8e497f6649309a
yas <105318763+yasmin07lynx@users.noreply.github.com>
2024-10-22 07:41:33 -0300

    tghn

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -5,7 +5,7 @@ async function visualizarInformacoesGlobais (){
 const res = await fetch (url)
 const dados = await res.json()
 const paragrafo = document.createElement ('p')
-paragrafo.classList.add('graficos-container_texto')
+paragrafo.classList.add('graficos-container__texto')
 paragrafo.innerHTML = `Voce sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectados} estão conectados em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
 
 const container = document.getElementById( 'graficos-container')
