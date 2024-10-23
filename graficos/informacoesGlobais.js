961ab7a74310ad8956355b6f76a61acabfa3f0c9
yas <105318763+yasmin07lynx@users.noreply.github.com>
2024-10-23 11:52:54 -0300

    XCVBN

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,16 +1,20 @@
 
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-async function visualizarInformacoesGlobais (){
-const res = await fetch (url)
-const dados = await res.json()
-const horas = parseInt (dados.tempo_medio)
-const minutos = Math.round ((dados.tempo_medio - horas) *100)
-const paragrafo = document.createElement ('p')
-paragrafo.classList.add('graficos-container__texto')
-paragrafo.innerHTML = `Voce sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectados} estão conectados em alguma rede social e passam em média ${dados.tempo_medio} horas e <span>${minutos}</span> minutos conectadas.`
+async function visualizarInformacoesGlobais() {
+        const res = await fetch(url)
+        const dados = await res.json()
+        const pessoaConectadas = (dados.total_pessoas_conectadas) / 1e9
+        const pessoasNoMundo = (dados.total_pessoas_mundo) / 1e9
+        const horas = parseInt (dados.tempo_medio)
+        const minutos = Math.round ((dados.tempo_medio - horas) * 100)
+        const paragrafo = document.createElement('p')
+        paragrafo.classList.add('graficos-container__texto')
+        paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões </span>
+        de pessoas e que aproximadamente <span>${pessoaConectadas} bilhões </span> estão 
+        conectadas em alguma rede social e passam em média <span>${horas} horas </span> e <span> ${minutos} minutos </span> conectadas.`
 
-const container = document.getElementById( 'graficos-container')
-container.appendChild(paragrafo)
-}
-visualizarInformacoesGlobais()
+        const container = document.getElementById ('graficos-container')
+        container.appendChild(paragrafo)
+    }
+    visualizarInformacoesGlobais()
