4ede665adaf9943cb21d2cdec29a0786afd65e9b
yas <105318763+yasmin07lynx@users.noreply.github.com>
2024-11-26 07:51:38 -0300

    sdfbn

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,18 +1,18 @@
 
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = 'https://raw.githubusercontent.com/yasmin07lynx/CienciaDeDados/refs/heads/main/BaseDeDados/esportes-dados-globais.json'
 
 async function visualizarInformacoesGlobais() {
         const res = await fetch(url)
         const dados = await res.json()
-        const pessoaConectadas = (dados.total_pessoas_conectadas) / 1e9
+        const pessoasAtletas = (dados.total_pessoas_que_praticam_esportes_regularmente) / 1e9
         const pessoasNoMundo = (dados.total_pessoas_mundo) / 1e9
         const horas = parseInt (dados.tempo_medio)
-        const minutos = Math.round ((dados.tempo_medio - horas) * 100)
-        const porcentagemConectada = ((pessoaConectadas / pessoasNoMundo) * 100).toFixed(2)
+        const minutos = Math.round ((dados.tempo_medio_semana_praticando_esportes - horas) * 100)
+        const porcentagemAtleta = ((pessoaAtleta / pessoasNoMundo) * 100).toFixed(2)
         const paragrafo = document.createElement('p')
         paragrafo.classList.add('graficos-container__texto')
         paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões </span>
-        de pessoas e que aproximadamente <span>${pessoaConectadas} bilhões </span> estão 
+        de pessoas e que aproximadamente <span>${pessoasAtletas} bilhões </span> estão 
         conectadas em alguma rede social e passam em média <span>${horas} horas </span> e <span> ${minutos} minutos </span> conectadas.`
 
         const container = document.getElementById ('graficos-container')
--- graficos/quantidadeDeUsuarios.js
+++ graficos/quantidadeDeUsuarios.js
@@ -1,7 +1,7 @@
 import { getCSS, tickConfig } from "./common.js"
 
 async function quantidadeUsuariosPorRede() {
-    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const url = 'https://raw.githubusercontent.com/yasmin07lynx/CienciaDeDados/refs/heads/main/BaseDeDados/esportes-mais-praticados.json'
     const res = await fetch(url)
     const dados = await res.json()
     const nomeDasRedes = Object.keys(dados)
