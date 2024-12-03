4d97a63e4f9e214f56b95c7c48e122424be4e65a
yas <105318763+yasmin07lynx@users.noreply.github.com>
2024-12-03 07:44:20 -0300

    fgthj

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -8,7 +8,6 @@ async function visualizarInformacoesGlobais() {
         const pessoasNoMundo = (dados.total_pessoas_mundo) / 1e9
         const horas = parseInt (dados.tempo_medio)
         const minutos = Math.round ((dados.tempo_medio_semana_praticando_esportes - horas) * 100)
-        const porcentagemAtleta = ((pessoaAtleta / pessoasNoMundo) * 100).toFixed(2)
         const paragrafo = document.createElement('p')
         paragrafo.classList.add('graficos-container__texto')
         paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões </span>
